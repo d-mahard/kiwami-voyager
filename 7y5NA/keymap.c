1716f32b2ca9a8b69e9eeab1854bbf60b8e8f6fd
Dipta Mahardhika <dipta.mahardhika.phd@gmail.com>
2025-08-14 19:26:47 +0200

    fix incorrect sym layer key

--- 7y5NA/keymap.c
+++ 7y5NA/keymap.c
@@ -23,7 +23,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    KC_ESCAPE,      ST_MACRO_0,     KC_MINUS,       KC_LPRN,        KC_LCBR,        OSL(3),                                         CLEAR_MODS,     KC_SLASH,       KC_HASH,        KC_AT,          KC_COLN,        KC_ENTER,       
+    KC_ESCAPE,      ST_MACRO_0,     KC_MINUS,       KC_LPRN,        KC_LCBR,        OSL(2),                                         CLEAR_MODS,     KC_SLASH,       KC_HASH,        KC_AT,          KC_COLN,        KC_ENTER,       
     OSM(MOD_LGUI),  KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           OSM(MOD_LALT),  
     DUAL_FUNC_0,    KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_QUES,        OSL(3),         
     MT(MOD_LALT, KC_DELETE),KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_EXLM,        KC_BSPC,        
